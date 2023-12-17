cee3b1464dbcdb038230b44f38ba32701edbab77
jimmyDev <jimmybuidev@gmail.com>
2023-12-16 16:33:32 -0800

    baseURL based on .env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from './UserContext';
 import Routes from './Routes';
 
 function App() {
-  axios.defaults.baseURL= 'http://localhost:4040';
+  axios.defaults.baseURL= import.meta.env.VITE_BACKEND_URL;
   axios.defaults.withCredentials= true;
 
   return(
