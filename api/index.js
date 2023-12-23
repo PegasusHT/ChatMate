96c344df446ebc9d44e542355573152647a4d821
jimmyDev <jimmybuidev@gmail.com>
2023-12-22 16:54:33 -0800

    added Bot model

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const User = require('./models/User');
+const Bot = require('./models/Bot');
 const Message = require('./models/Message');
 const jwt = require('jsonwebtoken');
 const cors =require('cors');
