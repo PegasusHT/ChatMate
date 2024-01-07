9c12f73ab22dedebfdad3f6e32a1e311969fb0d8
jimmyDev <jimmybuidev@gmail.com>
2024-01-06 23:25:20 -0800

    added url from local production

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL,
+  origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
 }));
 
 async function getUserDataFromRequest(req) {
