14e8006c34b20aa589ff1a73d373bec212c6b07f
jimmyDev <jimmybuidev@gmail.com>
2024-01-11 14:41:03 -0800

    log env

--- api/index.js
+++ api/index.js
@@ -25,8 +25,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+  origin: [process.env.CLIENT_URL],
 }));
+console.log('cors', process.env.CLIENT_URL);
 
 async function getUserDataFromRequest(req) {
   return new Promise((resolve, reject) => {
